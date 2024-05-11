7aac98bef72a2a32d56ab6949b7c237df06e7f60
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-05-11 00:00:17 +0000

    generate receipt

--- lib/pages/delivery_page.dart
+++ lib/pages/delivery_page.dart
@@ -13,12 +13,39 @@ class _DeliveryProgressPageState extends State<DeliveryProgressPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Delivery in progress"),
+        title: const Text("Delivery in progress"),
         backgroundColor: Colors.transparent,
       ),
-      body: Column(children: [
-        MyReceipt(),
-      ]),
+      bottomNavigationBar: _buildBottomNavBar(context),
+      body: const Column(
+        children: [
+          MyReceipt(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomNavBar(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      child: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.background,
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/pages/my_receipt.dart
+++ lib/pages/my_receipt.dart
@@ -18,16 +18,21 @@ class MyReceipt extends StatelessWidget {
               height: 25,
             ),
             Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Theme.of(context).colorScheme.secondary),
-                    borderRadius: BorderRadius.circular(8)),
-                padding: const EdgeInsets.all(25),
-                child: Consumer<Restaurant>(
-                  builder: (context, restaurant, child) {
-                    return Text(restaurant.displayCartReceipt());
-                  },
-                ))
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Theme.of(context).colorScheme.secondary),
+                  borderRadius: BorderRadius.circular(8)),
+              padding: const EdgeInsets.all(25),
+              child: Consumer<Restaurant>(
+                builder: (context, restaurant, child) {
+                  return Text(restaurant.displayCartReceipt());
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            const Text('Estimated delivery time is : 4:10 PM')
           ],
         ),
       ),
